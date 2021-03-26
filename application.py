4aad5df1c54052771fdf99881ba9454d900bb115
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-03-26 07:11:53 +0100

    Added routing

--- application.py
+++ application.py
@@ -1,3 +1,13 @@
 from flask import Flask
 from flask import render_template
 app = flask(__name__)
+
+
+@app.route('/')
+@app.route("/home")
+
+def home:
+  return render_template('home.html', title = SmartScan)
+
+if __name__ == '__main__':
+  app.run(debug = true)
